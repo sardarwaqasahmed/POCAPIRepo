4a6b7020dd681d2a8bdf52d6b1148f78fa3e25df
architect_pakistan@hotmail.com <architect_pakistan@hotmail.com>
2018-11-24 17:04:56 +0400

    removing the log filter from webclient

--- NYTimesAPI/src/main/java/com/search/api/service/impl/NyTimesClientImpl.java
+++ NYTimesAPI/src/main/java/com/search/api/service/impl/NyTimesClientImpl.java
@@ -1,16 +1,9 @@
 package com.search.api.service.impl;
 
-import java.nio.charset.Charset;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.search.api.config.AppProperties;
@@ -28,9 +21,7 @@ import reactor.core.publisher.Mono;
 @
 */
 @Service
-public class NyTimesClientImpl implements NyTimesClient {
-	
-	private static final Logger log = LoggerFactory.getLogger(NyTimesClientImpl.class);
+public class NyTimesClientImpl implements NyTimesClient {	
 
 	private static final String MIME_TYPE = "application/json";
 	private final String uri = "/mostviewed/{section}/{period}.json?apikey={key}";
@@ -44,28 +35,8 @@ public class NyTimesClientImpl implements NyTimesClient {
 		this.webClient = WebClient
 								.builder()
 								.baseUrl(appProperties.getAPI_BASE_URL())
-								.filter(logFilter())
 								.defaultHeader(HttpHeaders.CONTENT_TYPE, MIME_TYPE)
 								.build();
-	}
-
-	private ExchangeFilterFunction logFilter() {
-	    return (request, next) -> {
-	        log.debug("===========================request begin================================================");
-            log.debug("URI         : {}", request.url());
-            log.debug("Method      : {}", request.method());
-            log.debug("Headers     : {}", request.headers());
-            log.debug("Request body: {}", request.body().toString());
-            log.debug("==========================request end================================================");
-	        Mono<ClientResponse> response = (Mono<ClientResponse>) next.exchange(request);
-	        log.debug("============================response begin==========================================");
-            log.debug("Status code  : {}", response);
-//            log.debug("Status text  : {}", response.getStatusText());
-//            log.debug("Headers      : {}", response.getHeaders());
-//            log.debug("Response body: {}", StreamUtils.copyToString(response.getBody(), Charset.defaultCharset()));
-            log.debug("=======================response end=================================================");
-	        return response;
-	    };
 	}
 	    
 	@Override
